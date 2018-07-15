ff1eeb946bcfe72c6dfa9c259d5a38eaf9e82ab1
Supadech Premaboot <supadech@dezainwerkz.com>
2018-07-15 08:48:14 +0700

    Fix menu

--- assets/scenes/Menu.js
+++ assets/scenes/Menu.js
@@ -55,8 +55,8 @@ class Menu extends Phaser.Scene {
           this.input_name.length && this.player_name.destroy();
           this.input_name.push(i);
           this.player_name = this.add.text(
-            20,
-            250,
+            window.outerWidth / 2 - 160,
+            150,
             String.fromCharCode.apply(null, this.input_name),
             {
               fontSize: 24
@@ -91,7 +91,7 @@ class Menu extends Phaser.Scene {
         this.loading = false;
       } else {
         createText("You r new! Enter ya name!", 1);
-        createText("_________________________", 2);
+        createText("_________________________", 3);
         // show input name
       }
     });
