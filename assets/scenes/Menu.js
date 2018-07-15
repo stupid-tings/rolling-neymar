9d66c7b738f7da5c91c9f67f6a5871fd2a681adf
Supadech Premaboot <supadech@dezainwerkz.com>
2018-07-15 09:37:39 +0700

    input name

--- assets/scenes/Menu.js
+++ assets/scenes/Menu.js
@@ -1,3 +1,5 @@
+const MAX_LENGTH = 14;
+
 class Menu extends Phaser.Scene {
   constructor() {
     super({
@@ -7,10 +9,12 @@ class Menu extends Phaser.Scene {
   preload() {
     this.load.image("button", "assets/sprites/button.png");
     this.load.image("background", "assets/background/start.jpg");
+    this.load.image("logo", "assets/sprites/game-logo.png");
     this.loading = true;
   }
 
   create() {
+    localStorage.removeItem("player");
     const width = window.outerWidth;
     const height = window.outerHeight;
     const player = localStorage.getItem("player");
@@ -29,6 +33,8 @@ class Menu extends Phaser.Scene {
         this.key_enter.isDown = false;
       }
     });
+    const logo = this.add.image(width / 2, 80, "logo");
+    logo.setScale(0.5);
   }
 
   update(delta) {
@@ -40,10 +46,11 @@ class Menu extends Phaser.Scene {
       this.input_name.pop();
       this.player_name = this.add.text(
         window.outerWidth / 2 - 160,
-        150,
+        224,
         String.fromCharCode.apply(null, this.input_name),
         {
-          fontSize: 24
+          fontSize: 24,
+          backgroundColor: "#000"
         }
       );
       backspace.isDown = false;
@@ -51,15 +58,35 @@ class Menu extends Phaser.Scene {
     for (let i = 65; i >= 65 && i <= 90; i++) {
       const key = this.input.keyboard.addKey(i);
       if (!this.player && key.isDown) {
+        if (this.input_name.length <= MAX_LENGTH) {
+          this.input_name.length && this.player_name.destroy();
+          this.input_name.push(i);
+          this.player_name = this.add.text(
+            window.outerWidth / 2 - 160,
+            224,
+            String.fromCharCode.apply(null, this.input_name),
+            {
+              fontSize: 24,
+              backgroundColor: "#000"
+            }
+          );
+        }
+        key.isDown = false;
+      }
+    }
+    for (let i = 48; i >= 48 && i <= 57; i++) {
+      const key = this.input.keyboard.addKey(i);
+      if (!this.player && key.isDown) {
         if (this.input_name.length <= 24) {
           this.input_name.length && this.player_name.destroy();
           this.input_name.push(i);
           this.player_name = this.add.text(
             window.outerWidth / 2 - 160,
-            150,
+            224,
             String.fromCharCode.apply(null, this.input_name),
             {
-              fontSize: 24
+              fontSize: 24,
+              backgroundColor: "#000"
             }
           );
         }
@@ -68,32 +95,39 @@ class Menu extends Phaser.Scene {
     }
     if (this.key_enter.isDown) {
       if (this.input_name.length) {
-        this.player = String.fromCharCode.apply(null, this.input_name);
+        // this.player = String.fromCharCode.apply(null, this.input_name);
         localStorage.setItem("player", this.player);
+        // this.showName(this.player);
         this.loading = false;
+        this.scene.start("Game");
       }
     }
   }
 
   showName(player) {
+    const middle = window.outerWidth / 2 - 160;
+    const createText = (text, posY) =>
+      this.add.text(middle, 150 + posY * 25, text, {
+        fontSize: 24,
+        fontFamily: "Sans-serif"
+      });
+    if (!player) {
+      this.text1 = createText("You r new! Enter ya name!", 1);
+      this.text2 = createText("_________________________", 3);
+    } else {
+      if (this.text1 || this.text2) {
+        this.text1.destroy();
+        this.text2.destroy();
+        this.player_name.destroy();
+      }
+      createText(`Hello ${player}`, 2);
+    }
     dbRef.child(`${player}`).once("value", s => {
-      const middle = window.outerWidth / 2 - 160;
-      const createText = (text, posY) =>
-        this.add.text(middle, 100 + posY * 25, text, {
-          fontSize: 24,
-          fontFamily: "Sans-serif"
-        });
       if (s.exists()) {
         const scoreText = `Your high score :: ${s.val()}`;
-        createText(`Hello ${player}`, 1);
         createText(scoreText, 2);
-        this.player = player;
-        this.loading = false;
-      } else {
-        createText("You r new! Enter ya name!", 1);
-        createText("_________________________", 3);
-        // show input name
       }
+      this.loading = false;
     });
   }
 }
