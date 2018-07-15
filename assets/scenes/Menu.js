be0be1b6da78af822cd1e1bc812c2d110659a9fe
Supadech Premaboot <supadech@dezainwerkz.com>
2018-07-15 07:46:18 +0700

    :sparkles: scoreboard

--- assets/scenes/Menu.js
+++ assets/scenes/Menu.js
@@ -7,6 +7,7 @@ class Menu extends Phaser.Scene {
   preload() {
     this.load.image("button", "assets/sprites/button.png");
     this.load.image("background", "assets/background/start.jpg");
+    this.loading = true;
   }
 
   create() {
@@ -17,23 +18,13 @@ class Menu extends Phaser.Scene {
     this.key_enter = this.input.keyboard.addKey(
       Phaser.Input.Keyboard.KeyCodes.ENTER
     );
-    dbRef.child(`${player}`).once("value", s => {
-      if (s.exists()) {
-        const scoreText = `Your last score : ${s.val()}`;
-        this.add.text(0, 0, scoreText, { fontSize: 30 });
-        this.player = player;
-      } else {
-        this.add.text(20, 200, "You r new! Enter ya name!", { fontSize: 24 });
-        this.add.text(20, 260, "_________________________", { fontSize: 24 });
-        // show input name
-      }
-    });
+
+    this.showName(player);
     this.input_name = [];
-    console.log(this.add);
     const button = this.add.sprite(width / 2, height - 200, "button");
     button.setInteractive();
     button.on("pointerdown", () => {
-      if (this.player) {
+      if (!this.loading && this.player) {
         this.scene.start("Game");
         this.key_enter.isDown = false;
       }
@@ -79,7 +70,30 @@ class Menu extends Phaser.Scene {
       if (this.input_name.length) {
         this.player = String.fromCharCode.apply(null, this.input_name);
         localStorage.setItem("player", this.player);
+        this.loading = false;
       }
     }
+  }
+
+  showName(player) {
+    dbRef.child(`${player}`).once("value", s => {
+      const middle = window.outerWidth / 2 - 160;
+      const createText = (text, posY) =>
+        this.add.text(middle, 100 + posY * 25, text, {
+          fontSize: 24,
+          fontFamily: "Sans-serif"
+        });
+      if (s.exists()) {
+        const scoreText = `Your last score : ${s.val()}`;
+        createText(`Hello ${player}`, 1);
+        createText(scoreText, 2);
+        this.player = player;
+        this.loading = false;
+      } else {
+        createText("You r new! Enter ya name!", 1);
+        createText("_________________________", 2);
+        // show input name
+      }
+    });
   }
 }
--- assets/scenes/Over.js
+++ assets/scenes/Over.js
@@ -5,7 +5,7 @@ class Over extends Phaser.Scene {
     });
   }
   preload() {
-    this.load.audio('cry', 'assets/sounds/cry.mp3')
+    this.load.audio("cry", "assets/sounds/cry.mp3");
     this.load.image("over_bg", "assets/background/over.jpg");
   }
 
@@ -23,22 +23,46 @@ class Over extends Phaser.Scene {
       50,
       "Press Enter to bring Him home",
       {
-        fontSize: 36
+        fontSize: 36,
+        fontFamily: "Sans-serif"
       }
     );
     this.key_enter = this.input.keyboard.addKey(
       Phaser.Input.Keyboard.KeyCodes.ENTER
     );
-    this.cry = this.sound.add('cry')
-    this.cry.play()
+    this.cry = this.sound.add("cry");
+    this.cry.play();
     const player = localStorage.getItem("player");
     dbRef.child(player).set(parseInt(scores));
+    dbRef
+      .orderByValue()
+      .limitToLast(3)
+      .once("value", s => {
+        console.log(s.val());
+        this.topThree = s.val();
+      });
   }
 
   update() {
     if (this.key_enter.isDown) {
-      this.scene.start("Menu");
-      this.key_enter.isDown = false;
+      // this.scene.start("Menu");
+      // this.key_enter.isDown = false;
+      window.location.reload();
+    }
+    if (this.topThree) {
+      this.add.text(20, 20, "LEADER BORED", { fontSize: 36 });
+      Object.keys(this.topThree)
+        .sort((a, b) => this.topThree[b] - this.topThree[a])
+        .map((player_name, i) => {
+          const createText = (text, posX) =>
+            this.add.text(posX, 24 * (i + 3), `${text}`, {
+              fontSize: 24,
+              fontFamily: "Sans-serif"
+            });
+
+          createText(player_name, 10);
+          createText(this.topThree[player_name], 280);
+        });
     }
   }
 }
